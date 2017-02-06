f99a0c40bcec12f7b68fb5f62240006735777e58
unknown <duneval@gmail.com>
2017-02-06 20:56:38 +0100

    Added new game method and Random object for game.

--- src/zajecia/Zajecia5/Zajecia5.java
+++ src/zajecia/Zajecia5/Zajecia5.java
@@ -3,17 +3,86 @@ package zajecia.Zajecia5;
 import zajecia.czwarte.ZadaniaZeScannerem;
 import zajecia.czwarte.Zajecia4;
 
+import java.util.Random;
+
 /**
  * Created by RENT on 2017-02-06.
  */
 public class Zajecia5 {
     public static void main(String[] args) {
-        int number = ZadaniaZeScannerem.getNumberFromUser();
-        square(number);
-        triangle(number);
-        tree(number);
-        oneMatrix(number);
-        evenMatrix(number);
+//        int number = ZadaniaZeScannerem.getNumberFromUser();
+//        square(number);
+//        triangle(number);
+//        tree(number);
+//        oneMatrix(number);
+//        evenMatrix(number);
+//        System.out.println(sumOfNumbersFromUsers());
+//        printArray(arrayOfNumbersFromUser());
+        Random random = new Random();
+        int iloscKrokowDoZwyciestwa = game(random.nextInt(100));
+        System.out.println("Koniec gry");
+        System.out.println("Wygrales w " + iloscKrokowDoZwyciestwa + " krokach.");
+    }
+
+    public static int game(int number) {
+        boolean flag = true;
+        int counter = 0;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser();
+            counter++;
+            if (numberFromUser == number) {
+                flag = false;
+            } else if (numberFromUser < number) {
+                System.out.println("Za mala liczba.");
+            } else {
+                System.out.println("Za duza liczba");
+            }
+        }
+        return counter;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static int[] arrayOfNumbersFromUser() {
+        int[] tmpArray = new int[100];
+        int size = 0;
+        boolean flag = true;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser();
+            if (numberFromUser != 0) {
+                tmpArray[size] = numberFromUser;
+                size++;
+            } else {
+                flag = false;
+            }
+        }
+        return revriteArray(tmpArray, size);
+    }
+
+    public static int[] revriteArray(int[] bigArray, int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = bigArray[i];
+        }
+        return array;
+    }
+
+    public static int sumOfNumbersFromUsers() {
+        int sum = 0;
+        boolean flag = true;
+        while (flag) {
+            int numberFromUser = ZadaniaZeScannerem.getNumberFromUser("Insert next number: ");
+            if (numberFromUser != 0) {
+                sum += numberFromUser;
+            } else {
+                flag = false;
+            }
+        }
+        return sum;
     }
 
     public static void oneMatrix(int number) {
